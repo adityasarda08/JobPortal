d9fc01f98eceb0a21926e123db7fc5509f2915cd
aditya <adityasarda08.com>
2026-07-07 12:53:33 +0530

    final commit

--- Backend/index.js
+++ Backend/index.js
@@ -7,47 +7,71 @@ const jobRoutes = require('./routes/jobRoutes');
 const Companyroutes = require('./routes/Companyroutes');
 const applicationroutes = require('./routes/applicationroutes');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 
 const app = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Adjust this to your frontend URL
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-
+    origin:'http://localhost:5173',
+    credentials:true
 }));
 
-// Middleware
 app.use(express.json());
 app.use(cookieParser());
 
-// Connect to MongoDB
-connectDB();
 
-
-// Routes
-app.use('/api/users', userRoutes);
-app.use('/api/jobs', jobRoutes);
-app.use('/api/companies', Companyroutes);
+app.use('/api/users',userRoutes);
+app.use('/api/jobs',jobRoutes);
+app.use('/api/companies',Companyroutes);
 app.use('/api/applications',applicationroutes);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Job Portal API');
+
+
+if(process.env.NODE_ENV === 'production'){
+
+    const dirpath = path.resolve();
+
+    app.use(express.static(
+        path.join(dirpath,'frontend','dist')
+    ));
+
+    app.get('/*',(req,res)=>{
+        res.sendFile(
+            path.resolve(
+                dirpath,
+                'frontend',
+                'dist',
+                'index.html'
+            )
+        );
+    });
+}
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to Job Portal API");
 });
 
-app.use((req, res) => {
-    res.status(404).json({ message: 'Route not found' });
+
+app.use((req,res)=>{
+    res.status(404).json({
+        message:"Route not found"
+    });
 });
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Something went wrong!' });
-});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+
+
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server running on port ${PORT}`);
+    });
+})
+.catch(err=>{
+    console.log(err);
 });
